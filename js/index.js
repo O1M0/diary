b418bcb3865269f8b1ec4a92e153775a3b77de2e
O1M0 <o1m0.info.jp@gmail.com>
2025-12-10 00:50:07 +0900

    [remove]smoothScroll.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 // js/index.js
 import { initLoading } from './modules/loading.js';
-import { enableSmoothScroll } from './modules/smoothScroll.js';
 import { initFloatingSquares } from './modules/background.js';
 import { initHeaderScroll } from './modules/headerScroll.js';
 import { initMainVisualText } from './modules/mainVisualText.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initLoading();
-  enableSmoothScroll();
   initFloatingSquares();
   initHeaderScroll();
   initMainVisualText();
